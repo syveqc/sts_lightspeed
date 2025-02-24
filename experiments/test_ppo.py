c1d868f2c28b6b0413e5c395445b29b07f4cb83d
Tobias Kietreiber <tobias.kietreiber@fhstp.ac.at>
2025-02-24 13:04:50 +0000

    feat: loading models by command line args in PPO

--- experiments/test_ppo.py
+++ experiments/test_ppo.py
@@ -9,47 +9,38 @@ import sys
 import jax.numpy as jnp
 
 import gymnasium as gym
-
-class Model(nnx.Module):
-    def __init__(self, din, dout, rngs: nnx.Rngs):
-        self.linear1 = nnx.Linear(din, 512, rngs=rngs)
-        self.batch_norm1 = nnx.BatchNorm(512, rngs=rngs)
-        self.dropout1 = nnx.Dropout(0.2, rngs=rngs)
-        self.linear2 = nnx.Linear(512, 512, rngs=rngs)
-        self.linear3 = nnx.Linear(512, 512, rngs=rngs)
-        self.linear4 = nnx.Linear(512, 512, rngs=rngs)
-        self.linear5 = nnx.Linear(512, 512, rngs=rngs)
-        self.linear6 = nnx.Linear(512, dout, rngs=rngs)
-
-    def __call__(self, x):
-        x = jnp.expand_dims(jnp.array(x, dtype=jnp.float32), 1)
-        x = nnx.relu(self.linear1(x))
-        x = self.dropout1(self.batch_norm1(x))
-        x = nnx.relu(self.linear2(x))
-        x = nnx.relu(self.linear3(x))
-        x = nnx.relu(self.linear4(x))
-        x = nnx.relu(self.linear5(x))
-        return self.linear6(x)  # type: ignore
+import importlib
 
 input_size = 20
 
+checkpoint_dir = sys.argv[1]
+model_name = sys.argv[2]
+
+import_filename = f"data.train_{model_name}"
+module = importlib.import_module(import_filename)
+
 checkpointer = ocp.Checkpointer(ocp.StandardCheckpointHandler())
-model = nnx.eval_shape(lambda: Model(1, input_size, rngs=nnx.Rngs(0)))
+model = nnx.eval_shape(lambda: module.Model(1, input_size, rngs=nnx.Rngs(0)))
 _, _, abstract_state = nnx.split(model, nnx.RngState, ...)
-state_restored = checkpointer.restore(f"{sys.argv[1]}/pure_dict", abstract_state)
+state_restored = checkpointer.restore(f"{checkpoint_dir}/{model_name}", abstract_state)
 nnx.update(model, state_restored)
 model.eval()
 
 def mock_embedding(card: int):
     return np.array([card/370.0], np.float32)
 
-env = StsFightEnv(sts.CharacterClass.IRONCLAD, 0, 15, 5, model, 20)
+def embedding_wrapper(x):
+    x = jnp.expand_dims(x, axis=1)
+    x = model(x)
+    return x
+
+env = StsFightEnv(sts.CharacterClass.IRONCLAD, 0, 15, 5, embedding_wrapper, 20)
 env = gym.wrappers.TimeLimit(env, 1000)
 print(env.observation_space)
 obs, _ = env.reset()
 print(obs.shape)
 
-model = PPO("MlpPolicy", env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1, device='cpu', tensorboard_log=f"ppo_log/{model_name}")
 model.learn(total_timesteps=250000)
 model.save("ppo_sts")
 
